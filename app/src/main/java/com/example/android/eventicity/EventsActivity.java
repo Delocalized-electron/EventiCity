fc3fe571e5e266656874fcda6b5584639d7ac634
delocalized-electron <aadil.khatri343@gmail.com>
2018-08-18 17:26:45 +0530

    Launcher icon and updated menu
    added launcher icon and customized navigation menu

--- app/src/main/java/com/example/android/eventicity/EventsActivity.java
+++ app/src/main/java/com/example/android/eventicity/EventsActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import android.os.Handler;
 
 public class EventsActivity extends AppCompatActivity {
 
@@ -17,14 +20,14 @@ public class EventsActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_today:
+                    mTextMessage.setText(R.string.title_today);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_upcoming:
+                    mTextMessage.setText(R.string.title_upcoming);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
                     return true;
             }
             return false;
@@ -39,6 +42,31 @@ public class EventsActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    /**
+     *  double tap to exit
+     */
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                finish();
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 
 }
